1caf2460a1afcca2b95915f171c57071226bbb55
Christopher Markieta <markietachristopher@gmail.com>
2015-09-11 15:25:07 -0400

    Added default 0

--- ddl.sql
+++ ddl.sql
@@ -8,9 +8,9 @@ DROP TABLE IF EXISTS probability;
 CREATE TABLE IF NOT EXISTS probability
 (
     id                                              INT             AUTO_INCREMENT PRIMARY KEY,
-    probability_of_initial_knowledge                DECIMAL(11, 10) NOT NULL,
-    individualized_probability_of_initial_knowledge DECIMAL(11, 10) NOT NULL,
-    probability_of_learning                         DECIMAL(11, 10) NOT NULL,
-    probability_of_guess                            DECIMAL(11, 10) NOT NULL,
-    probability_of_slip                             DECIMAL(11, 10) NOT NULL
+    probability_of_initial_knowledge                DECIMAL(11, 10) NOT NULL DEFAULT 0,
+    individualized_probability_of_initial_knowledge DECIMAL(11, 10) NOT NULL DEFAULT 0,
+    probability_of_learning                         DECIMAL(11, 10) NOT NULL DEFAULT 0,
+    probability_of_guess                            DECIMAL(11, 10) NOT NULL DEFAULT 0,
+    probability_of_slip                             DECIMAL(11, 10) NOT NULL DEFAULT 0
 );
